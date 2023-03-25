c276a9038acea8ebe39ec4480adbb630d5af3b82
GetOutOfSync <tannerjhansen24@gmail.com>
2023-03-25 11:12:51 -0500

    Enabled source control on the example project.

---
+++ bin/ProgramExample.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using dice_roller;
+
+Dice dice = new Dice();
+ChanceResult[] resultTable = { new ChanceResult(2, "Cat"), new ChanceResult(3, "Dog"), new ChanceResult(1, "Fish"), new ChanceResult(1, "Snake") };
+ChanceTable table = new ChanceTable("example", resultTable);
+
+Console.Write(JsonConvert.SerializeObject(table, Formatting.Indented));
