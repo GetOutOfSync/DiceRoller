c87a453b7db77dc788c41931be3c015d31522f0a
GetOutOfSync <tannerjhansen24@gmail.com>
2023-03-25 11:24:15 -0500

    Fleshed out the example program with commands and console write stuff. May need a better way to find the file later.

--- bin/ProgramExample.cs
+++ bin/ProgramExample.cs
@@ -4,6 +4,10 @@ using dice_roller;
 
 Dice dice = new Dice();
 ChanceResult[] resultTable = { new ChanceResult(2, "Cat"), new ChanceResult(3, "Dog"), new ChanceResult(1, "Fish"), new ChanceResult(1, "Snake") };
-ChanceTable table = new ChanceTable("example", resultTable);
+ChanceTable table = ChanceTable.FromJson(FileWorker.ReadFile("../../../../example/example.json"));
 
-Console.Write(JsonConvert.SerializeObject(table, Formatting.Indented));
+Console.WriteLine("Basic 1d6 roll with Dice.Roll(6): " + dice.Roll(6));
+Console.WriteLine("Basic 2d6 roll with Dice.RollMany(2, 6): " + dice.RollMany(2, 6));
+Console.WriteLine("Basic percentile roll with Dice.RollPercent(): " + dice.RollPercent());
+Console.WriteLine("Parsing a string '2d6+1' into a roll with Dice.ParseString('2d6+1'): " + dice.ParseString("2d6+1"));
+Console.WriteLine("Rolling on a chance table with Dice.RollOnTable(table): " + dice.RollOnTable(table));
